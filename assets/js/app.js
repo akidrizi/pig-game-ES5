9d1ff27ef2fe682069013bf7862aab4a27a46f5e
Akis Idrizi <a.intrizi@gmail.com>
2018-11-15 10:06:23 +0200

    Reset player names after starting new game

--- assets/js/app.js
+++ assets/js/app.js
@@ -63,6 +63,9 @@ function startGame() {
 	document.querySelector(".player-1-panel").classList.remove("active");
 	document.querySelector(".player-0-panel").classList.add("active");
 
+	document.querySelector("#name-0").textContent = 'Player 1!';
+	document.querySelector("#name-1").textContent = 'Player 2!';
+	
 	document.querySelector(".dice").src = "assets/images/dice-1.png";
 }
 
